a1d015805248efc095ae7929f3f21746abab70e6
Graham-Stein <graham@guavoaviation.co.uk>
2018-10-23 17:36:07 +0100

    Add start of loop thoughts

--- src/views/planet_info_view.js
+++ src/views/planet_info_view.js
@@ -6,17 +6,22 @@ const PlanetInfoView = function () {
 
 PlanetInfoView.prototype.bindEvent = function () {
   PubSub.subscribe('SolarSystem:planet-info', (event) => {
-    console.log("PLANET INFO? ",event);
-    console.log("PLANET INFO? ",event.detail);
-    console.log("PLANET INFO? ",event.detail.name);
+    // console.log("PLANET INFO? ",event);
+    // console.log("PLANET INFO? ",event.detail);
+    // console.log("PLANET INFO? ",event.detail.name);
     const planetDisplayInfo = event.detail;
     this.displayResult(planetDisplayInfo);
   });
 };
 
 PlanetInfoView.prototype.displayResult = function (planet_details) {
-  const resultElement = document.querySelector('.planet-details');
-    resultElement.textContent = planet_details
+  const resultElement = document.querySelector('#planet_data');
+    // resultElement.textContent =
+    console.log(planet_details);
+    planet_details.forEach(function(detail) {
+      console.log(detail);
+      resultElement.textContent = detail;
+    });
 };
 
 module.exports = PlanetInfoView;
